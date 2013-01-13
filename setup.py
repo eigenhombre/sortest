3594af4efb0fc40bbaf665854150256b679ff384
John Jacobsen <john@mail.npxdesigns.com>
2013-01-13 16:36:53 -0600

    Version bump for last commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='sortest',
-      version='0.0.7',
+      version='0.0.8',
       description=('Continuous testing in Python with sorting by test speed '
                    'and auto-restart when files change'),
       url='https://github.com/eigenhombre/sortest',
